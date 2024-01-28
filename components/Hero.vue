09ae388303371c76e12ac6e2001e7dd76baec3b2
soybean15 <marlonpadilla1593@gmail.com>
2024-01-28 20:30:10 +0800

    test image

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="">
-        <div class="grid max-w-screen-xl px-6 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 m-24">
+        <div class="grid max-w-screen-xl px-6 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 m-24 ">
             <div class="mr-auto place-self-center lg:col-span-7">
                 <h1
                     class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-base-content z-10">
@@ -11,7 +11,7 @@
                
                     <Stacks/>
                     <a href="#"
-                    class="inline-flex items-center justify-center px-5 py-3 mr-3 font-medium text-center text-base-content rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                    class="inline-flex items-center justify-center px-5 py-3 mr-3 mt-5 font-medium text-center text-base-content rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                     See Projects
                     <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
                         xmlns="http://www.w3.org/2000/svg">
@@ -24,8 +24,8 @@
                 
 
             </div>
-            <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-                <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup">
+            <div class="hidden lg:mt-0 lg:col-span-5 lg:flex bg-base-300 rounded-full ">
+                <img  class="object-top object-contain hover:object-scale-down " src="~/assets/img/mob.png" alt="mockup">
             </div>
         </div>
     </section>
