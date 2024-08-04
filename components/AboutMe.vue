2fb7478bbdc352b61914112ae19ec78ed29f7678
soybean15 <marlonpadilla1593@gmail.com>
2024-08-04 11:26:50 +0800

    Update fixed broken image on projuxt thumbnail

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -24,8 +24,8 @@
             <div class="space-y-3 border-b-[1px] border-b-accent py-3 pb-5" v-for="me in myInfo" :key="me.title">
                 <div class="text-xl  md:text-2xl font-bold  text-accent">{{ me.title }}</div>
 
-                    <about-item :me="me"/>
-               
+                <about-item :me="me" />
+
 
             </div>
 
@@ -40,72 +40,110 @@ import AboutItem from './AboutItem.vue'
 
 
 const myInfo = [
-    { title: 'Full Name', content: [
-     {
-            title: 'Marlon Padilla',
-          
-        }
-    ] },
-    { title: 'Current Address', content: [
-        {
-            title: 'Peñaranda, Nueva Ecija',
-          
-        }
-    ] },
-    { title: 'Academic Background', content: [
-        {
-            title: 'Bachelor of Science in Information Technology',
-            position: 'Nueva Ecija University of Science and Technology (Aug 2020-Aug 2024)',
-        }
-    ] },
+    {
+        title: 'Full Name', content: [
+            {
+                title: 'Marlon Padilla',
+
+            }
+        ]
+    },
+    {
+        title: 'Current Address', content: [
+            {
+                title: 'Peñaranda, Nueva Ecija',
+
+            }
+        ]
+    },
+    {
+        title: 'Academic Background', content: [
+            {
+                title: 'Bachelor of Science in Information Technology',
+                position: 'Nueva Ecija University of Science and Technology (Aug 2020-Aug 2024)',
+            }
+        ]
+    },
 
     {
         title: 'Professional Background', content: [
             // 'Currently a student pursuing Bachelor of Science in Information Technology with a focus on Web System Technology.',
             // 'Actively engaged in coursework related to Web Development.',
             // 'Currently interning at Gleent Inc. as a Laravel Developer.',
-           
-           { 
-            title: 'Gleent Inc.',
-            position: 'Laravel Dev Intern (Jan 2024-Apr 2024)',
 
-           
-            content :[
-                {
-                    subtitle: "Assigned Projects",
-                   
-                    content:[
-                        'Barangay Information System',
-                        'ERP SaaS',
-                        'Task Management'
-                    ],
+            {
+                title: 'Gleent Inc.',
+                position: 'Laravel Dev Intern (Jan 2024-Apr 2024)',
 
-                }
-            ]
-           },
-           { 
-            title: 'Gleent Inc.',
-            position: 'Laravel Dev (Apr 2024- Present)',
-            content :[
-                {
-                    subtitle: "Assigned Projects",
-                    content:[
-                        'ERP SaaS',
-                        'HRIS and Payroll',
-                        'Task Management'
-                    ],
 
-                }
-            ]
-           }
-            
+                // content :[
+                //     {
+                //         subtitle: "Assigned Projects",
+
+                //         content:[
+                //             'Barangay Information System',
+                //             'ERP SaaS',
+                //             'Task Management'
+                //         ],
+
+                //     }
+                // ]
+            },
+            {
+                title: 'Gleent Inc.',
+                position: 'Laravel Dev (Apr 2024- Present)',
+                // content :[
+                //     {
+                //         subtitle: "Assigned Projects",
+                //         content:[
+                //             'ERP SaaS',
+                //             'HRIS and Payroll',
+                //             'Task Management'
+                //         ],
+
+                //     }
+                // ]
+            },
+
+
         ]
+
     },
-  
+    {
+        "title": "Work Projects",
+        "content": [
+            {
+                "title": "Barangay Information System",
+                "content": [
+                    "A comprehensive system for managing resident data, streamlining administrative tasks, and improving service delivery for local government units."
+                ]
+            },
+            {
+                "title": "SaaS Management",
+                "content": [
+                    "A platform enhancing user experience and operational efficiency for client organizations."
+                ]
+            },
+            {
+                "title": "Task Management",
+                "content": [
+                    "A system for assigning and tracking tasks for staggered or on-call employees, ensuring efficient workload distribution and management."
+                ]
+            },
+            {
+                "title": "HRIS and Payroll",
+                "content": [
+                    "A Human Resource Information System (HRIS) with integrated payroll functionalities, automating HR processes and ensuring accurate payroll management."
+                ]
+            }
+        ]
+    }
+
+
 ]
 
 export default {
-  components: { AboutItem },
+    components: { AboutItem },
 
     setup() {
 
